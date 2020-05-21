8f59c758b3417736d544434b9d4088b8a1671c4f
KangWang <szwkang@cn.ibm.com>
2020-05-21 18:12:02 +0800

    del ignore

---
+++ springcloud/democonfig/src/test/java/cn/ibm/com/democonfig/DemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ibm.com.democonfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
