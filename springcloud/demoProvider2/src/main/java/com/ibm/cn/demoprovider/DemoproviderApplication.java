d88d158cad0a00c52e80bdce96f253e236b2bbc8
KangWang <szwkang@cn.ibm.com>
2020-05-22 16:33:54 +0800

    cash

--- springcloud/demoProvider2/src/main/java/com/ibm/cn/demoprovider/DemoproviderApplication.java
+++ springcloud/demoProvider2/src/main/java/com/ibm/cn/demoprovider/DemoproviderApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
 public class DemoproviderApplication {
 
--- springcloud/demoProvider2/src/main/java/com/ibm/cn/demoprovider/controller/UserController.java
+++ springcloud/demoProvider2/src/main/java/com/ibm/cn/demoprovider/controller/UserController.java
@@ -2,37 +2,28 @@ package com.ibm.cn.demoprovider.controller;
 
 import com.ibm.cn.demoprovider.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Random;
 
 @RestController
 @RequestMapping(value = "/user")
+@RefreshScope
 public class UserController {
 
     @Autowired
     private User user;
 
-    @GetMapping(value = "/getUserByName")
-    public User getUserByName(@RequestParam(value = "name") String name) {
-        int sleepTime = new Random().nextInt(3000);
-        try {
-            System.out.println("随机时间：" + sleepTime);
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if ("Tracy".equals(name)) {
-            user.setName("Tracy");
-            user.setSex("male");
-            user.setAge(28);
-            user.setPhone("110");
-        } else {
-            user.setName("LH");
-            user.setSex("femal");
-            user.setAge(23);
-            user.setPhone("156");
-        }
-        return user;
+    @Value("${user.name}")
+    private String name;
+
+    @GetMapping(value = "/getUser")
+    public User getUser() {
+        User demo =new User();
+        demo.setName(name);
+        demo.setAge(user.getAge());
+        demo.setPhone(user.getPhone());
+        demo.setSex(user.getSex());
+        return demo;
     }
 }
