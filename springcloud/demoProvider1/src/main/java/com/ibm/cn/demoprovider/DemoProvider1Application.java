a22b91b96fbfa9a42b1196bb33396f839ad8e3e3
KangWang <szwkang@cn.ibm.com>
2020-05-24 23:01:04 +0800

    modify the file name

---
+++ springcloud/demoProvider1/src/main/java/com/ibm/cn/demoprovider/DemoProvider1Application.java
@@ -0,0 +1,15 @@
+package com.ibm.cn.demoprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DemoProvider1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProvider1Application.class, args);
+    }
+}
---
+++ springcloud/demoProvider2/src/main/java/com/ibm/cn/demoprovider/DemoProvider2Application.java
@@ -0,0 +1,18 @@
+package com.ibm.cn.demoprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DemoProvider2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProvider2Application.class, args);
+    }
+
+}
